61e7d4aa2a22d2476e8fc84160b0cd9d44662495
Steve Githinji <124651060+stevegithinji@users.noreply.github.com>
2023-07-10 09:27:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,44 +9,41 @@ import json
 app = Flask(__name__)
 
 # helper function here
-def news_classification(text):
+
+def iris_prediction(sepal_length, sepal_width, petal_length, petal_width):
     """
     Given sepal length, sepal width, petal length, and petal width,
     predict the class of iris
     """
-    
+
     # Load the model from the file
-    with open("model.pkl", "rb") as f:
-        baseline_model = joblib.load(f)
+    with open("model1.pkl", "rb") as f:
+        model = joblib.load(f)
 
-    # Vectorize the new text using the same vectorizer
-    new_text_vectorized = tfidf.transform([text])
+    # Construct the 2D matrix of values that .predict is expecting
+    X = [[sepal_length, sepal_width, petal_length, petal_width]]
 
-    # Make predictions using the trained model
-    predicted_label = baseline_model.predict(new_text_vectorized)
-    
-    return {"predicted_label": predicted_label}
+    # Get a list of predictions and select only 1st
+    predictions = model.predict(X)
+    prediction = int(predictions[0])
+
+    return {"predicted_class": prediction}
 
 # defining routes here
+
 @app.route('/', methods=['GET'])
 def index():
-    return 'Heyy!'
+    return 'Hello, world!'
 
 @app.route('/predict', methods=['POST'])
-def predict(request):
-    """
-    `request` is an HTTP request object that will automatically be passed
-    in by Google Cloud Functions
-    
-    You can find all of its properties and methods here:
-    https://flask.palletsprojects.com/en/1.0.x/api/#flask.Request
-    """
-    
+def predict():
     # Get the request data from the user in JSON format
     request_json = request.get_json()
-    
+
+    # We are expecting the request to look like this:
+    # {"sepal_length": <x1>, "sepal_width": <x2>, "petal_length": <x3>, "petal_width": <x4>}
     # Send it to our prediction function using ** to unpack the arguments
-    result = news_classification(**request_json)
-    
+    result = iris_prediction(**request_json)
+
     # Return the result as a string with JSON format
     return json.dumps(result)
